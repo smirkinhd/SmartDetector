256a41975d92c27703ce8779f7744fcb73466799
smirkinhd <smirkinhd@gmial.com>
2025-01-05 20:27:28 +0500

    feat: подключение к React

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using BackendGermanSmartDetector.AppDbContext;
+﻿using BackendGermanSmartDetector.AppDbContext;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +6,11 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5040);
+});
 
 var configuration = builder.Configuration;
 
@@ -26,40 +31,19 @@ builder.Services.AddAuthentication(options =>
 });
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-
 var app = builder.Build();
 
-if (builder.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
-}
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwagger(options =>
-    {
-        options.SerializeAsV2 = true;
-    });
-}
+app.UseStaticFiles();
 
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
